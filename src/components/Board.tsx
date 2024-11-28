fd252465fe613e6ac246e8c9e165d20d71e3bfb3
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 14:25:22 +0100

    Update <Board /> to handle navigating to tasks and be able to handle task operations (add, update, delete) via TaskManager

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,36 @@
-import { Card } from "@components/Card";
 import { Column } from "@components/Column";
-import { initialBoardList } from "@src/data/boardEntries";
+import { BoardProp, TaskProp } from "@src/data/boardEntries";
+import { TaskManager } from "@src/data/taskManager";
+import { Dispatch, SetStateAction } from "react";
+import { useNavigate } from "react-router";
 
-export function Board() {
+type Props = {
+	boards: BoardProp[];
+	setBoards: Dispatch<SetStateAction<BoardProp[]>>;
+};
+
+export function Board({ boards, setBoards }: Props) {
+	const navigate = useNavigate();
+	const handleAddTask = (boardId: string): void => {
+		const newTask = TaskManager.addTask(boardId, {
+			title: "",
+			content: "",
+			boardId
+		});
+		setBoards(TaskManager.getAllBoards());
+		navigate(`/board/${boardId}/task/${newTask.id}`);
+	};
+
+	const handleUpdateTask = (boardId: string, taskId: number, updates: Partial<TaskProp>): void => {
+		TaskManager.updateTask(boardId, taskId, updates);
+		setBoards(TaskManager.getAllBoards());
+	};
+
+	const handleDeleteTask = (boardId: string, taskId: number): void => {
+		TaskManager.deleteTask(boardId, taskId);
+		setBoards(TaskManager.getAllBoards());
+	};
+
 	return (
 		<div className="flex flex-col h-full max-g-full sm:max-h-[66vh] bg-gray-200">
 			<div className="px-10 mt-6">
@@ -10,16 +38,19 @@ export function Board() {
 			</div>
 			<div className="flex-grow overflow-auto mx-4 mb-4 pt-4 space-x-6">
 				<div className="flex md:flex-row flex-col flex-shrink-0 gap-4">
-					{initialBoardList.map((board) => (
-						<Column key={board.id} title={board.title}>
-							{board.tasks.map((task, idx) => (
-								<Card key={idx + 1} title={task.title} content={task.content} />
-							))}
-						</Column>
+					{boards.map((board) => (
+						<Column
+							key={board.id}
+							columnId={board.id}
+							title={board.title}
+							tasks={board.tasks}
+							onCreateTask={() => handleAddTask(board.id)}
+							onUpdateTask={handleUpdateTask}
+							onDeleteTask={handleDeleteTask}
+						/>
 					))}
-
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
