3f7bd88ce67aecfc676bee80c2c9040d898fe306
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 15:46:42 +0100

    Update App to use <Provider /> and <PersistGate /> from redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Layout } from "@layout/Layout";
+import { persistor, store } from "@src/store/store";
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router';
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
-
-
 	return (
-		<BrowserRouter>
-			<Layout />
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<Layout />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	);
 }
 
