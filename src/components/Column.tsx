1227b28d066eb29737baa8ec4e34f22c1c15fa3a
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 14:24:08 +0100

    Update <Column /> to accept create, update, delete tasks

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,24 +1,53 @@
+import { Card } from "@components/Card";
 import { DownCaret } from "@src/assets/icons/DownCaret";
-import { ReactNode } from "react";
+import { TaskProp } from "@src/data/boardEntries";
+
 type Props = {
 	title: string;
-	children: ReactNode;
+	tasks: TaskProp[];
+	columnId: string;
+	onCreateTask: () => void;
+	onUpdateTask: (boardId: string, taskId: number, updates: Partial<TaskProp>) => void;
+	onDeleteTask: (boardId: string, taskId: number) => void;
 };
-export function Column({ children, title }: Props) {
+export function Column({
+	columnId,
+	title,
+	tasks,
+	onCreateTask,
+	onUpdateTask,
+	onDeleteTask
+}: Props) {
 	return (
 		<details
 			className="group/column flex flex-col flex-shrink-0 w-full md:w-72"
 			open={true}
+			data-column-id={columnId}
 		>
 			<summary className="flex justify-between items-center flex-shrink-0 h-10 px-2">
 				<div className="flex items-center">
 					<h2 className="block text-sm font-semibold">{title}</h2>
-					<span className="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">6</span>
+					<span className="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">
+						{tasks.length}
+					</span>
 				</div>
 				<div className="flex items-center">
-					<button className="flex items-center justify-center w-6 h-6 text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100">
-						<svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+					<button
+						onClick={onCreateTask}
+						className="flex items-center justify-center w-6 h-6 text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100"
+					>
+						<svg
+							className="w-5 h-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+							></path>
 						</svg>
 					</button>
 					<div>
@@ -27,8 +56,17 @@ export function Column({ children, title }: Props) {
 				</div>
 			</summary>
 			<div className="flex flex-col pb-2 overflow-auto">
-				{children}
+				{tasks.map((task, idx) => (
+					<Card
+						key={`${task.id}`}
+						task={task}
+						columnId={columnId}
+						index={idx}
+						onUpdate={onUpdateTask}
+						onDelete={onDeleteTask}
+					/>
+				))}
 			</div>
 		</details>
-	)
+	);
 }
