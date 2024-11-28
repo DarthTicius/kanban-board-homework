3ae6ae8ab43a5e1cfc38ae450ee1cc51df30091e
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 15:48:48 +0100

    Update <Layout /> to handle our useAppSelector hook

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,19 +2,13 @@ import { Board } from "@src/components/Board";
 import { Footer } from "@src/components/Footer";
 import { Header } from "@src/components/Header";
 import { TaskDetails } from "@src/components/TaskDetails";
-import { BoardProp } from "@src/data/boardEntries";
-import { TaskManager } from "@src/data/taskManager";
+import { useAppSelector } from "@src/hooks/redux";
 import "@style/app.scss";
-import { useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router";
 
 export function Layout() {
 	const navigate = useNavigate();
-	const [boards, setBoards] = useState<BoardProp[]>(() => TaskManager.getAllBoards());
-
-	useEffect(() => {
-		TaskManager.saveBoards(boards);
-	}, [boards]);
+	const boards = useAppSelector((state) => state.boards.boards);
 
 	return (
 		<div className="max-w-screen-2xl mx-auto">
