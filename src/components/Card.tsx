98d83728f1ad1ef5b9f112c93e2da274021b8752
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 14:38:52 +0100

    Fix double touch on smaller screens (mobile) on <Card />

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import { Calendar } from "@src/assets/icons/Calendar";
 import { CloseIcon } from "@src/assets/icons/Close";
 import { TaskProp } from "@src/data/boardEntries";
+import { useRef, useState, type TouchEvent } from "react";
 import { useNavigate } from "react-router";
 
 type CardProps = {
@@ -21,18 +22,40 @@ export function Card({ task, columnId, index, onUpdate, onDelete }: CardProps) {
 	const handleEdit = () => {
 		navigate(`/board/${columnId}/task/${task.id}`);
 	};
+	const touchTimeout = useRef<ReturnType<typeof setTimeout>>();
+	const [touchCount, setTouchCount] = useState(0);
+
+	const handleTouch = (e: TouchEvent) => {
+		e.preventDefault();
+		setTouchCount(prev => prev + 1);
+
+		if (touchTimeout.current) {
+			clearTimeout(touchTimeout.current);
+		}
+
+		if (touchCount === 1) {
+			handleEdit();
+			setTouchCount(0);
+		} else {
+			touchTimeout.current = setTimeout(() => {
+				setTouchCount(0);
+			}, 300);
+		}
+	};
+
 
 	return (
 		<div
 			className="group/card relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 hover:bg-opacity-100"
 			draggable="true"
 			onDoubleClick={handleEdit}
+			onTouchStart={handleTouch}
 		>
 			<button className="absolute top-0 right-0 hidden items-center justify-center w-5 h-5 mt-3 mr-2 text-gray-500 rounded group-hover/card:bg-gray-200 group-hover/card:text-gray-700 group-hover/card:flex">
 				<CloseIcon
 					className="w-4 h-4 fill-current"
-					onClick={() => {
-						console.log("delete");
+					onClick={(e) => {
+						e.stopPropagation();
 						onDelete(columnId, task.id);
 					}}
 				/>
