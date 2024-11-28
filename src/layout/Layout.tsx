1e2362794a60cf9c104d248c8101a409f289908e
DusanPetkovic <senator.dusan@gmail.com>
2024-11-28 14:20:15 +0100

    Move elements to <Layout />

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,23 @@
+import { Board } from "@src/components/Board";
+import { Footer } from "@src/components/Footer";
+import { Header } from "@src/components/Header";
+import { BoardProp } from "@src/data/boardEntries";
 import "@style/app.scss";
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router";
+export function Layout() {
 
 type Props = {
 	children: React.ReactNode;
 };
 export function Layout({ children }: Props) {
+
 	return (
 		<div className="max-w-screen-2xl mx-auto">
+			<Header />
+			<Board boards={boards} setBoards={setBoards} />
 			{children}
+			<Footer />
 		</div>
 	)
 }
