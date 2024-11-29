7882a74b36d647912d88581a5e78ff4c1492e3d3
DusanPetkovic <senator.dusan@gmail.com>
2024-11-29 12:37:35 +0100

    Add 'movTask' to slice

--- src/store/slices/boardSlice.ts
+++ src/store/slices/boardSlice.ts
@@ -1,3 +1,4 @@
+import { UniqueIdentifier } from "@dnd-kit/core";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { BoardProp, initialBoardList, TaskProp } from "@src/data/boardEntries";
 
@@ -83,8 +84,59 @@ const boardSlice = createSlice({
 				);
 			}
 		},
+		moveTask: (
+			state,
+			action: PayloadAction<{
+				activeId: UniqueIdentifier;
+				overId: UniqueIdentifier;
+				sourceBoardId: string;
+				destinationBoardId: string;
+			}>,
+		) => {
+			const { activeId, overId, sourceBoardId, destinationBoardId } =
+				action.payload;
+
+			const sourceBoardIndex = state.boards.findIndex(
+				(board) => board.id === sourceBoardId,
+			);
+			const destinationBoardIndex = state.boards.findIndex(
+				(board) => board.id === destinationBoardId,
+			);
+
+			if (sourceBoardIndex === -1 || destinationBoardIndex === -1) return;
+
+			const sourceBoard = state.boards[sourceBoardIndex];
+			const destinationBoard = state.boards[destinationBoardIndex];
+
+			const taskToMove = sourceBoard.tasks.find((task) => task.id === activeId);
+			if (!taskToMove) return;
+
+			sourceBoard.tasks = sourceBoard.tasks.filter(
+				(task) => task.id !== activeId,
+			);
+			const overTaskIndex = destinationBoard.tasks.findIndex(
+				(task) => task.id === overId,
+			);
+
+			const destinationIndex =
+				overTaskIndex !== -1 ? overTaskIndex : destinationBoard.tasks.length;
+
+			if (sourceBoardId !== destinationBoardId) {
+				taskToMove.boardId = destinationBoardId;
+			}
+			destinationBoard.tasks.splice(destinationIndex, 0, {
+				...taskToMove,
+				order: destinationIndex,
+			});
+			destinationBoard.tasks = destinationBoard.tasks.map((task, index) => ({
+				...task,
+				order: index,
+			}));
+
+			localStorage.setItem("kanbanBoards", JSON.stringify(state.boards));
+		},
 	},
 });
 
-export const { addTask, updateTask, deleteTask } = boardSlice.actions;
+export const { addTask, updateTask, deleteTask, moveTask } = boardSlice.actions;
 export default boardSlice.reducer;
